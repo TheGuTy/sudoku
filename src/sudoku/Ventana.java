d3ce73e1f62789dff079835c1857dea2748a69b1
Gabriel Gutierrez <nggutierrez.45@gmail.com>
2017-05-08 21:22:07 -0300

    primer enlace java prolog

--- src/sudoku/Ventana.java
+++ src/sudoku/Ventana.java
@@ -1,11 +1,44 @@
 package sudoku;
 
-public class Ventana {
+import java.awt.Color;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+import org.jpl7.Query;
+
+public class Ventana extends JFrame {
+
+	public Ventana() {
 		
-		System.out.println("hola mundo!");
+		super("Sudoku");
+		
+		cargarLogica();
+	}
+	
+	private void cargarLogica() {
+		
+		String consulta = "consult('logicasudoku.pl')";
+		Query cargarLogica = new Query(consulta);
+		
+		if (!cargarLogica.hasSolution()) {
+			JOptionPane.showMessageDialog(null, "Error al cargar la lógica del sudoku");
+		} 
+	}
+
+	public static void main(String[] args) {		
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				Ventana inst = new Ventana();
+				inst.setSize(470,600);
+				inst.setResizable(false);
+				inst.setBackground(Color.BLACK);
+				inst.setLocationRelativeTo(null);
+				inst.setVisible(true);
+			}
+		});
 
 	}
 
